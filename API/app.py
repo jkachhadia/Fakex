1aa9ce94dfc1d9c29c95aa918f4cf6438f6743dd
jkachhadia <jaykachhadia@hotmail.com>
2017-11-21 22:42:14 +0530

    created virtual env and initialized apis creation

---
+++ API/app.py
@@ -0,0 +1,22 @@
+import time
+import os
+import sys
+import requests
+import json
+from flask import Flask,request
+
+app=Flask(__name__)
+
+@app.route('/api/<query>', methods=['GET','POST'])
+def fknscore(query):
+    return "ok", 200
+
+
+
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print str(message)
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
