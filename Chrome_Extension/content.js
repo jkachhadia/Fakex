af418e4f29c945d37705c54f215e154a0a42eb73
Nash Vail <nishantve1@gmail.com>
2018-05-09 10:29:10 +0530

    Added score visibility

--- Chrome_Extension/content.js
+++ Chrome_Extension/content.js
@@ -16,7 +16,15 @@ const scoreThresholds = {
   verified: 1
 };
 
-const errorBGColor = '#000';
+const scoreNames = {
+  fake: 'Fake',
+  partiallyFake: 'Partially Fake',
+  ambiguous: 'Ambiguous',
+  partiallyVerified: 'Partially Verified',
+  verified: 'Verified'
+};
+
+const errorBGColor = '#ff0000';
 
 const gotMessage = (message, sender, sendResponse) => {
 
@@ -27,7 +35,7 @@ const gotMessage = (message, sender, sendResponse) => {
   fetch(url + text)
     .then(response => response.json())
     .then(jsonData => {
-      setBackgroundColor(containerNode, computeColorFromScore(jsonData.score));
+      setBackgroundColor(containerNode, computeColorFromScore(jsonData.score), jsonData.score);
     });
 };
 
@@ -52,11 +60,34 @@ const computeColorFromScore = (score) => {
     console.log('Error');
     return errorBGColor;
   }
-
 };
 
-const setBackgroundColor = (node, color) => {
+const getScoreName = (score) => {
+  if (score < scoreThresholds.fake) {
+    return scoreNames.fake;
+  } else if (score > scoreThresholds.fake && score < scoreThresholds.partiallyFake) {
+    return scoreNames.partiallyFake;
+  } else if (score > scoreThresholds.partiallyFake && score < scoreThresholds.ambiguous) {
+    return scoreNames.ambiguous;
+  } else if (score > scoreThresholds.ambiguous && score < scoreThresholds.partiallyVerified) {
+    return scoreNames.partiallyVerified;
+  } else if (score > scoreThresholds.partiallyVerified && score < scoreThresholds.verified) {
+    return scoreNames.verified;
+  } else {
+    return errorBGColor;
+  }
+};
+
+const setBackgroundColor = (node, color, score) => {
   node.style.background = color;
+  node.classList.add('fakex-target');
+
+
+  let scoreNode = document.createElement('div');
+  scoreNode.textContent = `${Math.round(score*100)}% - ${getScoreName(score)}`;
+  scoreNode.style.backgroundColor = color;
+  scoreNode.classList.add('fakex-score-container');
+  node.appendChild(scoreNode);
 };
 
 chrome.runtime.onMessage.addListener(gotMessage);
